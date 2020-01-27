3c86a6c3b8d2c51d3868bc455402273abce47c48
Artuom16RUS <artyomjava@gmail.com>
2020-01-27 10:18:47 +0300

    Added Benchmark

---
+++ src/test/java/service/ServiceTriggerTest.java
@@ -0,0 +1,30 @@
+package service;
+
+import model.User;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceTriggerTest {
+
+    @org.junit.jupiter.api.Test
+    void getDataChanges() {
+        SQLRequest request = new SQLRequest();
+        ServiceTrigger trigger = new ServiceTrigger();
+
+        int countOne = request.insert("A", 1000, "B");
+        assertEquals(1, countOne);
+
+        int countTwo = request.update(1, "none", 0, "none");
+        assertEquals(1, countTwo);
+
+        int countThree = request.delete(1);
+        assertEquals(1,countThree);
+
+        int sizeArray = trigger.getDataChanges("2020-01-27").size();
+        assertEquals(3, sizeArray);
+
+        request.close();
+    }
+}
---
+++ src/test/java/service/TriggerBenchmark.java
@@ -0,0 +1,23 @@
+package service;
+
+import com.carrotsearch.junitbenchmarks.AbstractBenchmark;
+import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
+import org.junit.Test;
+
+
+public class TriggerBenchmark extends AbstractBenchmark {
+
+    @BenchmarkOptions(benchmarkRounds = 20, warmupRounds = 10)
+    @Test
+    public void getDataChanges() {
+        SQLRequest request = new SQLRequest();
+        ServiceTrigger trigger = new ServiceTrigger();
+        int count = 1_000;
+
+        for (int i = 0; i < count; i++) {
+            request.insert("Abcdefghijklmnopqrstuvwxyz", count, "Abcdefghijklmnopqrstuvwxyz");
+        }
+        trigger.getDataChanges("2020-01-27");
+
+    }
+}
